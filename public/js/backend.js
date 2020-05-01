3fff6a13346bbadce768ce5dbc9d76d1d8a28b9e
Tommy Danielsson <tdserviceco@gmail.com>
2020-05-01 17:44:53 +0200

    changed host adress

--- public/js/backend.js
+++ public/js/backend.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
     value = $(this).serializeArray();
     console.log("array data: ", value)
 
-    // const socket = io.connect('http://localhost:3000');
-    const socket = io.connect('https://xboxfighter-scoreboard.herokuapp.com');
+    const socket = io.connect('http://localhost:3000');
+    // const socket = io.connect('https://xboxfighter-scoreboard.herokuapp.com');
     socket.emit('server', { client: value });
   })
 
