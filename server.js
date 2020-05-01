1e5a9aef4fc4d7d0245b038924aef6d59f810ad6
Tommy Danielsson <tdserviceco@gmail.com>
2020-05-01 17:44:09 +0200

    added a procfile

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/backend', function (req, res) {
 })
 
 http.listen(process.env.PORT || 3000, () => {
-  console.log('listening on *:3000');
+  console.log('listening on port X');
 });
 
 io.on('connection', (socket) => {
