a4957739b801136b2cec7f32f3993f427b043a81
Tommy Danielsson <tdserviceco@gmail.com>
2020-05-01 17:49:03 +0200

    will it work now?

--- public/js/backend.js
+++ public/js/backend.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
     value = $(this).serializeArray();
     console.log("array data: ", value)
 
-    const socket = io.connect('http://localhost');
-    // const socket = io.connect('https://xboxfighter-scoreboard.herokuapp.com');
+    // const socket = io.connect('http://localhost');
+    const socket = io.connect('https://xboxfighter-scoreboard.herokuapp.com');
     socket.emit('server', { client: value });
   })
 
