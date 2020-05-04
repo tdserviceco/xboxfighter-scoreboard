1229f73a0458a73ca0f3d6b1d7d4ec64633e3f0b
Tommy Danielsson <tdserviceco@gmail.com>
2020-05-04 14:41:38 +0200

    added a favicon at right place

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -117,14 +117,6 @@ $(document).ready(function () {
       .append('<meta charset="UTF-8">')
       .append('<meta name="viewport" content="width=device-width, initial-scale=1.0">')
       .append('<title>Xboxfighters Scoreboard</title>')
-      .append('<script src="/socket.io/socket.io.js"></script>')
-      .append('<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">')
-      .append('<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">')
-      .append('<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">')
-      .append('<link rel="manifest" href="/site.webmanifest">')
-      .append('<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">')
-      .append('<meta name="msapplication-TileColor" content="#da532c">')
-      .append('<meta name="theme-color" content="#0e7a0d">')
       .append('<link rel="stylesheet" href="../css/layouts/' + game + '.css">');
     }
   });
